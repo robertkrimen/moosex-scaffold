554ea7e27fc113e081285c90f776439a1e05f495
robertkrimen <robertkrimen@gmail.com>
2008-10-10 16:33:59 -0700

    Basically working

--- lib/MooseX/ClassScaffold.pm
+++ lib/MooseX/ClassScaffold.pm
@@ -5,7 +5,7 @@ use strict;
 
 =head1 NAME
 
-MooseX::ClassScaffold - The great new MooseX::ClassScaffold!
+MooseX::ClassScaffold -
 
 =head1 VERSION
 
@@ -15,42 +15,126 @@ Version 0.01
 
 our $VERSION = '0.01';
 
+require Exporter;
+use vars qw/@ISA @EXPORT/;
+@ISA = qw/Exporter/;
+@EXPORT = qw/Scaffold/;
+
+sub Scaffold { __PACKAGE__ }
+
+use Class::Inspector;
+use Carp;
+use Moose();
+use Moose::Exporter;
+use MooseX::ClassAttribute();
+
+no Moose;
+
+sub _load_scaffold_class {
+    my $self = shift;
+    my $scaffold_class = shift;
+    return if Class::Inspector->loaded($scaffold_class);
+    eval "require $scaffold_class;" or croak "Unable to load scaffold class $scaffold_class since: $@";
+}
+
+sub setup_scaffolding_import {
+    my $self = shift;
+    my %given = @_;
+
+    my $exporting_package = $given{exporting_package} ||= caller();
+    my $scaffold_class = $given{scaffold_class} ||= caller();
+
+    my ( $import, $unimport ) = $self->build_scaffolding_import(%given);
+
+    no strict 'refs';
+    *{ $exporting_package . '::import' }   = $import;
+}
+
+sub build_scaffolding_import {
+    my $self = shift;
+    my %given = @_;
+
+    my $scaffold_class = $given{scaffold_class} ||= caller();
+    $self->_load_scaffold_class($scaffold_class);
+
+    my $chain_import = $given{chain_import};
+
+    return sub {
+        my $CALLER = Moose::Exporter::_get_caller(@_);
+        my $class = shift;
+
+        # TODO Check to see if $CALLER is a Moose::Object?
+        $scaffold_class->SCAFFOLD($CALLER->meta, exporting_package => $class, @_);
+
+        goto &$chain_import if $chain_import;
+    };
+}
+
+sub load_or_scaffold_class {
+    my $self = shift;
+    my %given = @_;
+
+    my $class = $given{class};
+    my $scaffold_class = $given{scaffold_class};
+
+    unless (Class::Inspector->loaded($class)) {
+        if (Class::Inspector->installed($class)) {
+            eval "require $class;";
+            die $@ if $@;
+            # TODO Check to see if $class is a Moose::Object?
+            return $class->meta;
+        }
+    }
+
+    $self->_load_scaffold_class($scaffold_class);
+
+    my $meta = Moose::Meta::Class->create($class);
+
+    $scaffold_class->SCAFFOLD($meta, exporting_package => undef, %given);
+
+    return $meta;
+}
+
+sub extends {
+    my $self = shift;
+    my $class = shift;
+    $class = $class->name if ref $class;
+    return Moose::extends($class, @_);
+}
+
+sub has {
+    my $self = shift;
+    my $class = shift;
+    $class = $class->name if ref $class;
+    return Moose::has($class, @_);
+}
+
+sub class_has {
+    my $self = shift;
+    my $class = shift;
+    $class = $class->name if ref $class;
+
+    Moose::Util::MetaRole::apply_metaclass_roles(
+        for_class => $class,
+        metaclass_roles => [ 'MooseX::ClassAttribute::Role::Meta::Class' ],
+    );
+
+    return MooseX::ClassAttribute::class_has($class, @_);
+}
 
 =head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use MooseX::ClassScaffold;
-
-    my $foo = MooseX::ClassScaffold->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 FUNCTIONS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
 Robert Krimen, C<< <rkrimen at cpan.org> >>
+
+=head1 SOURCE
+
+You can contribute or fork this project via GitHub:
+
+L<http://github.com/robertkrimen/moosex-classscaffold/tree/master>
+
+    git clone git://github.com/robertkrimen/moosex-classscaffold.git MooseX-ClassScaffold
 
 =head1 BUGS
 
---
+++ t/01-basic.t
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use Test::Most;
+
+plan qw/no_plan/;
+
+package t::Scaffold::Object;
+
+use Moose;
+
+package t::Scaffold;
+
+use Test::More;
+
+use MooseX::ClassScaffold;
+
+MooseX::ClassScaffold->setup_scaffolding_import;
+
+sub SCAFFOLD {
+    my $self = shift;
+    my ($meta, %given) = @_;
+
+    Scaffold->extends($meta => "t::Scaffold::Object");
+    Scaffold->class_has($meta => apple => qw/is rw/);
+    Scaffold->class_has($meta => banana => qw/is rw/);
+
+    is($given{cherry}, 2);
+}
+
+package t::ScaffoldProject;
+
+use Moose;
+
+t::Scaffold->import(cherry => 2);
+
+package main;
+
+ok(t::ScaffoldProject->isa("t::Scaffold::Object"));
+ok(t::ScaffoldProject->apple(1));
+ok(!t::ScaffoldProject->banana(0));
+
+1;
